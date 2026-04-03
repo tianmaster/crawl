tianmaster/crawl

commit 049f8105fcea5526461fc929b8e9c53d076dacd3

Don't have marionette continue to treat charmed monsters as hostile
If a Aphotic Marionette changed some monster's loyalty (eg: by charming
it), the marionetted monster would happily keep blasting away at it with
subsequent spells anyway. Now they should treat it as friendly as soon as
it becomes so.

crawl-ref/source/god-abil.cc
@@ -2997,7 +2997,9 @@ static bool _marionette_spell_attempt(monster& caster, spell_type spell,
 
     for (monster* targ : targs)
     {
-        if (!targ->alive())
+        // We verify alignment again at this point, just in case it's changed
+        // in the middle of Marionette (eg: by the monster charming something).
+        if (!targ->alive() || targ->wont_attack())
             continue;
 
         caster.foe = targ->mindex();

commit 859e8e3ba0ee06dbb1f0c94883075464fe6de16b

Fix Marionette being unusuable on solo enemies (autumn)
Accidentally broken by 049f810. The fact that enemies could target
themselves here was needed to allow casting buffs/summons when solo.

But in an attempt to prevent just smiting themselves to death, try to
prevent casting attack spells on oneself. (This isn't 100% correct, but
hopefully adequate enough.)

crawl-ref/source/god-abil.cc
@@ -2992,13 +2992,22 @@ static bool _marionette_spell_attempt(monster& caster, spell_type spell,
                                       vector<monster*>& targs,
                                       bool check_only = false)
 {
-    shuffle_array(targs);
+    const spell_flags flags = get_spell_flags(spell);
+    const bool aggressive = (flags & (spflag::targeting_mask))
+                            && !(flags & ((spflag::helpful | spflag::escape)));
 
+    shuffle_array(targs);
     for (monster* targ : targs)
     {
+        // Don't cast attack spells directly on ourselves.
+        // (This is a very crude approximation, but in general we assume
+        // untargeted AoE already won't hurt its own caster.)
+        if (targ == &caster && aggressive)
+            continue;
+
         // We verify alignment again at this point, just in case it's changed
         // in the middle of Marionette (eg: by the monster charming something).
-        if (!targ->alive() || targ->wont_attack())
+        if (!targ->alive() || (targ->wont_attack() && targ != &caster))
             continue;
 
         caster.foe = targ->mindex();